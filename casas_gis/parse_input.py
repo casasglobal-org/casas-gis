d067ac23577bcaac31896e99f8003df58b3661bc
Luigi Ponti <quartese@gmail.com>
2021-07-01 12:11:33 +0200

    split csv_to_df and info into distinct functions

--- casas_gis/parse_input.py
+++ casas_gis/parse_input.py
@@ -1,19 +1,40 @@
-""" Read a tab-separated text file into a dictionary, return (numbered)
+""" Read a tab-separated text file into a pandas dataframe, return (numbered)
 column names, number of rows, and the dictionary itself.
 """
 import os
+import glob
 import pandas as pd
 
 
 INPUT_FOLDER = os.path.join(os.getcwd(), 'casas_gis/input_data')
 
 
-def get_ascii_table(filename, path=INPUT_FOLDER):
-    df = pd.read_csv(os.path.join(path, filename), sep='\t')
-    print(df.head())
+def csv_to_df(filepath):
+    return pd.read_csv(filepath, sep='\t')
+
+
+def get_df_info(df):
+    print(df.head(), '\n')
+    print('rows, columns:', df.shape, '\n')
     return df.info()
 
 
-get_ascii_table("Olive_30set19_00002.txt")
+# Deprecated as filepth and df are now two separate functions
+def get_ascii_table(filename, path=INPUT_FOLDER):
+    df = pd.read_csv(os.path.join(path, filename), sep='\t')
+    print(df.head(), '\n')
+    print('rows, columns:', df.shape, '\n')
+    return df.info()
 
-# def get_table_info(table):
+
+if __name__ == "__main__":
+    df_dict = {}
+    for filepath in glob.iglob(f'{INPUT_FOLDER}/*.txt'):
+        print(filepath)
+        df = csv_to_df(filepath)
+        get_df_info(df)
+        dict_name = os.path.splitext(os.path.basename(filepath))[0]
+        print(dict_name)
+        df_dict[dict_name] = df
+
+    pd.concat(df_dict.values(), ignore_index=True)
