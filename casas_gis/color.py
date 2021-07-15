ad059c983657004fbbf573cdaa6cbcd4b57dcac8
Luigi Ponti <quartese@gmail.com>
2021-07-15 11:17:28 +0200

    looked at mind map and created some files

---
+++ casas_gis/color.py
@@ -0,0 +1,2 @@
+""" Color related stuff.
+    Select color scheme, adjust to data, etc."""
---
+++ casas_gis/report.py
@@ -0,0 +1,2 @@
+""" Get an HTML report where results of the mapping and analysis are reported,
+    see e.g., https://jackbakerds.com/posts/python-equivalent-rmarkdown/ """
