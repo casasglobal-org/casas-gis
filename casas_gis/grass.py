0ed27badc7ace3c0c17c8a2a318bb0a3befde03f
Luigi Ponti <quartese@gmail.com>
2021-08-09 13:38:37 +0200

    code 2021-08-09 with bob (fix topmodule import)

--- casas_gis/grass.py
+++ casas_gis/grass.py
@@ -8,52 +8,17 @@
     https://baharmon.github.io/python-in-grass
     """
 
-
-""" GRASS enviornment
-https://grasswiki.osgeo.org/wiki/Working_with_GRASS_without_starting_it_explicitly
-
-See also issue #15 https://github.com/luisponti/casas-gis/issues/15
-
-# List of variables that GRASS modules need to run without starting
-# the main GRASS program
-
-export GISBASE=/Applications/GRASS-8.0.app/Contents/Resources
-export GISRC="$HOME.grass8"
-export PATH="$GISBASE/bin:$GISBASE/scripts:$PATH"
-export PYTHONPATH="$GISBASE/etc/python:$PYTHONPATH"
-
-export DYLD_LIBRARY_PATH="$GISBASE/lib:$DYLD_LIBRARY_PATH"
-export LD_LIBRARY_PATH="$GISBASE/lib:$LD_LIBRARY_PATH"
-export MANPATH="$MANPATH:$GISBASE/man"
-
-export GRASS_LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
-export GRASS_LD_LIBRARY_PATH="$DYLD_LIBRARY_PATH"
-
-If the GRASS libraries are shared libraries, the loader needs to be able 
-to find them. This normally means that LD_LIBRARY_PATH (Linux, Solaris), 
-DYLD_LIBRARY_PATH (MacOSX) or PATH (Windows) need to contain $GISBASE/lib
-
-"""
-
 import os
 import sys
-import atexit
 
-# Could the "module level import not at top of file" be fixed using
-# a different module that sets environmental variables somewhere else?
-# grassbin = "/Applications/GRASS-8.0.app/Contents/Resources/bin/grass"
-# os.environ['GRASSBIN'] = grassbin
-
-# https://pybit.es/articles/persistent-environment-variables/
 from dotenv import load_dotenv
-load_dotenv()
-grassbin = os.getenv("GRASSBIN")
 
-from grass_session import Session
-# see https://github.com/zarch/grass-session
-# http://osgeo-org.1560.x6.nabble.com/Using-grass-libraries-in-python-outside-of-GRASS-td5442894.html
-# http://osgeo-org.1560.x6.nabble.com/issue-with-grass-session-on-MacOSX-Cannot-find-GRASS-GIS-start-script-td5376619.html
-import grass.script as grass
+load_dotenv()  # needed for grass_session
+
+from grass_session import Session  # noqa E402
+import grass.script as grass  # noqa E402
+
+grassbin = os.getenv("GRASSBIN")
 
 # Clenaup routine?
 # See https://grasswiki.osgeo.org/wiki/Converting_Bash_scripts_to_Python
